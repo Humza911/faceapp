40b96a6eb96402a22f598b99829e1140a5f6d7ac
Humza Aamir <144220544+Humza911@users.noreply.github.com>
2024-01-18 16:17:08 +1100

    added interface elements and particles

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,9 +1,18 @@
 import React from "react";
+import './ImageLinkForm.css'
 
 const ImageLinkForm = () => 
 {
     return(
-        <div></div>
+        <div>
+            <p className="white f4">
+            {'This Magic Brain detects faces in pictures'}
+            </p>
+            <div className=" whitebox flex items-center">               
+                <input className="f4 pa2 w-80 mr2 shadow-1 bn" type="text"/>
+                <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple br2 bn">Detect</button>       
+            </div>
+        </div>
     )
 }
 
